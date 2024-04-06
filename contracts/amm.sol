b3ec679a100fafae49e38e5614f86b7858ef89d3
Joe Marron <marronjo@tcd.ie>
2024-04-06 18:58:05 +0100

    fix add liq timeout, less FHE operations in sqrt

--- contracts/amm.sol
+++ contracts/amm.sol
@@ -2,8 +2,8 @@
 pragma solidity >=0.8.13 <0.9.0;
 
 import {Permissioned, Permission} from "@fhenixprotocol/contracts/access/Permissioned.sol";
-import { IFHERC20 } from "./IFHERC20.sol";
-import { FHE, inEuint32, euint32, inEuint8, euint8, inEbool, ebool } from "@fhenixprotocol/contracts/FHE.sol";
+import {IFHERC20} from "./IFHERC20.sol";
+import {FHE, inEuint32, euint32, inEuint8, euint8, inEbool, ebool} from "@fhenixprotocol/contracts/FHE.sol";
 
 contract AMM is Permissioned {
 
@@ -36,12 +36,11 @@ contract AMM is Permissioned {
         ebool zeroForOne, 
         euint8 sellAmount
     ) external {
-
         //even if token0 is not being deposited, transfer 0 tokens to contract to obscure the trade direction
         euint8 token0Amount = FHE.select(zeroForOne, sellAmount, ZERO); //bad practice to use 'ZERO' in multiple places without re-encrypting?
         euint8 token1Amount = FHE.select(zeroForOne, ZERO, sellAmount);
-        //euint8 sellReserve = FHE.select(zeroForOne, s_liquidity0, s_liquidity1);
-        //euint8 buyReserve = FHE.select(zeroForOne, s_liquidity1, s_liquidity0);
+        euint8 sellReserve = FHE.select(zeroForOne, s_liquidity0, s_liquidity1);
+        euint8 buyReserve = FHE.select(zeroForOne, s_liquidity1, s_liquidity0);
 
         i_token0.transferFromEncrypted(
             msg.sender,
@@ -55,17 +54,16 @@ contract AMM is Permissioned {
             token1Amount
         );
 
-        //euint8 amountOut = (buyReserve * sellAmount) / (sellReserve + sellAmount);
+        euint8 amountOut = (buyReserve * sellAmount) / (sellReserve + sellAmount);
 
-        //settleLiquidity(zeroForOne, sellAmount, amountOut);
+        settleLiquidity(zeroForOne, sellAmount, amountOut);
 
         //swap token amounts now for withdrawal of bought tokens
         //even if 0 tokens are withdrawm, still update balance by 0 to obscure trade direction
-        //i_token0.transferEncrypted(msg.sender, FHE.asEuint32(token1Amount));
-        //i_token1.transferEncrypted(msg.sender, FHE.asEuint32(token0Amount));
+        i_token0.transferEncrypted(msg.sender, token1Amount);
+        i_token1.transferEncrypted(msg.sender, token0Amount);
 
-        //re encrypt return value with user pub key, so only user can decrypt with their private key
-        //return FHE.asEuint32(FHE.sealoutput(amountOut, userPublicKey));
+        //return FHE.asEuint8(FHE.sealoutput(amountOut, permission.publicKey));
     }
     
     //max amount of each side user is willing to deposit
@@ -74,7 +72,6 @@ contract AMM is Permissioned {
         inEuint8 calldata maxAmountIn0,
         inEuint8 calldata maxAmountIn1    
     ) external {
-
         euint8 maxAmount0 = FHE.asEuint8(maxAmountIn0);
         euint8 maxAmount1 = FHE.asEuint8(maxAmountIn1);
 
@@ -95,18 +92,17 @@ contract AMM is Permissioned {
         i_token0.transferFromEncrypted(msg.sender, address(this), optAmount0);
 
         optAmount1 = FHE.select(liquidity0or1Zero, maxAmount1, tempOptAmount1); 
-        //TODO - fix nonce error / headers timeout
-        i_token1.transferFromEncrypted(msg.sender, address(this), optAmount1); //throws nonce error!
+        i_token1.transferFromEncrypted(msg.sender, address(this), optAmount1);
 
-        //ebool totalSharesZero = FHE.eq(s_totalShares, ZERO);
+        ebool totalSharesZero = FHE.eq(s_totalShares, ZERO);
 
-        //euint8 poolShares = FHE.select(totalSharesZero, _sqrt(optAmount0 * optAmount1), calculateTotalSharesNonZeroLiquidity(optAmount0, optAmount1));
+        euint8 poolShares = FHE.select(totalSharesZero, _sqrt(optAmount0 * optAmount1), calculateTotalSharesNonZeroLiquidity(optAmount0, optAmount1));
 
         s_liquidity0 = s_liquidity0 + optAmount0;
         s_liquidity1 = s_liquidity1 + optAmount1;
 
-        //s_totalShares = s_totalShares + poolShares;
-        //s_userLiquidityShares[msg.sender] = s_userLiquidityShares[msg.sender] + poolShares;
+        s_totalShares = s_totalShares + poolShares;
+        s_userLiquidityShares[msg.sender] = s_userLiquidityShares[msg.sender] + poolShares;
     }
 
     // just used for testing, does not follow constant product formula
@@ -176,14 +172,31 @@ contract AMM is Permissioned {
         return FHE.select(cp0ltcp1, cp0 / s_liquidity0, maxAmount1);
     }
 
-    // function calculateTotalSharesNonZeroLiquidity(euint8 optAmount0, euint8 optAmount1) private view returns (euint8 poolShares){
-    //     euint8 shares0 = (optAmount0 * s_totalShares) / s_liquidity0;
-    //     euint8 shares1 = (optAmount1 * s_totalShares) / s_liquidity1;
+    function calculateTotalSharesNonZeroLiquidity(euint8 optAmount0, euint8 optAmount1) private view returns (euint8 poolShares){
+        euint8 shares0 = (optAmount0 * s_totalShares) / s_liquidity0;
+        euint8 shares1 = (optAmount1 * s_totalShares) / s_liquidity1;
 
-    //     ebool shares0ltShares1 = FHE.lt(shares0, shares1);
+        ebool shares0ltShares1 = FHE.lt(shares0, shares1);
 
-    //     poolShares = FHE.select(shares0ltShares1, shares0, shares1);
-    // }
+        poolShares = FHE.select(shares0ltShares1, shares0, shares1);
+    }
+
+    // less FHE operations than original method
+    function _sqrt(euint8 ye) private pure returns(euint8){
+        uint8 y = FHE.decrypt(ye);
+        uint8 z;
+        if (y > 3) {
+            z = y;
+            uint8 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+        return FHE.asEuint8(z);
+    }
 
     // function _sqrt(euint8 y) private view returns (euint8 z) {
     //     ebool ygt3 = FHE.gt(y, THREE);

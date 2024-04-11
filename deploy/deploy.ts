6d8091e7aaa78235fd01dd39fb69e7e11aa9aeab
Joe Marron <marronjo@tcd.ie>
2024-04-11 22:35:46 +0100

    edit deploy script for token0, token1, amm

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,4 +1,5 @@
 import { DeployFunction } from "hardhat-deploy/types";
+import { createFheInstance } from "../utils/instance";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import chalk from "chalk";
 
@@ -19,16 +20,32 @@ const func: DeployFunction = async function () {
     }
   }
 
-  const counter = await deploy("Counter", {
+  const token0 = await deploy("MyFHERC20", {
     from: signer.address,
-    args: [],
+    args: ["EToken0", "ETK0"],
     log: true,
     skipIfAlreadyDeployed: false,
   });
 
-  console.log(`Counter contract: `, counter.address);
+  const token1 = await deploy("MyFHERC20", {
+    from: signer.address,
+    args: ["EToken1", "ETK1"],
+    log: true,
+    skipIfAlreadyDeployed: false,
+  });
+
+  const amm = await deploy("AMM", {
+    from: signer.address,
+    args: [token0.address, token1.address],
+    log: true,
+    skipIfAlreadyDeployed: false,
+  });
+
+  console.log(`Token0 contract: `, token0.address);
+  console.log(`Token1 contract: `, token1.address);
+  console.log(`AMM contract: `, amm.address);
 };
 
 export default func;
-func.id = "deploy_counter";
-func.tags = ["Counter"];
+func.id = "deploy_amm";
+func.tags = ["AMM"];
